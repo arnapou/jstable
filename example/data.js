507a433d8f918131d125c4131b6bb25bb1a0a96d
Arnaud Buathier <arnaud.buathier@livingobjects.fr>
2013-07-26 17:12:22 +0200

    refactor data

--- example/data.js
+++ example/data.js
@@ -1,6 +1,7 @@
 var data = [
-	{id: 1, name: 'john', age: 20},
-	{id: 2, name: 'carol', age: 25},
-	{id: 3, name: 'alain', age: 22},
-	{id: 4, name: 'sue', age: 18}
+	{id: 211, name: 'john', age: 20},
+	{id: 452, name: 'carol', age: 25},
+	{id: 433, name: 'alain', age: 22},
+	{id: 511, name: 'georges', age: 22},
+	{id: 144, name: 'sue', age: 8}
 ];
